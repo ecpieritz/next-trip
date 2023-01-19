f233f9b7b3d089f0615fc0db671df9d816aa7dbf
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-01-19 12:11:42 -0300

    Added Navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,87 @@
+<template>
+  <nav class="navbar  navbar-expand-lg navbar-dark nt-color__bg__black-gradient">
+    <a class="navbar-brand" href="#"><span class="nt-color__text__pink">N</span>ext <span class="nt-color__text__pink">T</span>rip</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="nav justify-content-center collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Travels</a>
+        </li>
+      </ul>
+
+    </div>
+    <a class="nt-btn bg-pink" href="#nt-contact">Contact Us</a>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../scss/main.scss';
+
+.navbar{
+  padding: 20px 80px;
+  .navbar-brand{
+    font-weight: 800;
+    font-size: 40px;
+    color: $nt-white;
+  }
+  .nav-item{
+    &.active{
+      .nav-link{
+        color: $nt-blue;
+      }
+    }
+    .nav-link{
+      color: $nt-white;
+      font-size: 18px;
+      font-weight: 600;
+
+      &:hover{
+        color: $nt-blue;
+      }
+    }
+  }
+
+  .nav{
+    justify-content: space-between;
+  }
+
+  @media(max-width: 780px){
+    padding: 5px 10px;
+    .navbar-toggler{
+      order: 2;
+      border: none;
+    }
+    .nt-btn{
+      order: 1;
+    }
+    .navbar-brand{
+      order: 0;
+      font-size: 35px;
+    }
+
+    .justify-content-center{
+      justify-content: left !important;
+      margin: 20px 0;
+    }
+  }
+}
+</style>
