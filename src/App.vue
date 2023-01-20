26807f938b94fe1b4b314ee63a6676af1b91ba6b
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-01-20 13:18:18 -0300

    Added OurSupport

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
   <main>
     <AboutSection />
     <TravelCarousel />
+    <OurSupport />
   </main>
 </template>
 
@@ -12,11 +13,12 @@ import NavBar from './components/NavBar.vue'
 import HeroBanner from './components/HeroBanner.vue'
 import AboutSection from './components/AboutSection.vue'
 import TravelCarousel from './components/TravelCarousel.vue'
+import OurSupport from './components/OurSupport.vue'
 
 export default {
   name: 'App',
   components: {
-    NavBar, HeroBanner, AboutSection, TravelCarousel
+    NavBar, HeroBanner, AboutSection, TravelCarousel, OurSupport
   }
 }
 </script>
---
+++ src/components/OurSupport.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="nt-support">
+    <div class="container-fluid">
+      <h2 class="text-center">Do you want to prevent yourself in case something unforeseen arises on your trip?</h2>
+      <h3 class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porttitor nibh sit amet ornare gravida. In consequat malesuada vehicula. Donec maximus turpis id sapien gravida facilisis.</h3>
+
+      <div class="row">
+        <div class="col-lg-4">
+          <img src="../assets/imgs/icons/nt-support__call.png" alt="">
+          <h4>Support 24/7</h4>
+          <p>Suspendisse at nulla at tortor cursus feugiat. Praesent sed auctor felis.</p>
+        </div>
+        <div class="col-lg-4">
+          <img src="../assets/imgs/icons/nt-support__travel-insurance.png" alt="">
+          <h4>Travel insurance</h4>
+          <p>Suspendisse at nulla at tortor cursus feugiat. Praesent sed auctor felis.</p>
+        </div>
+        <div class="col-lg-4">
+          <img src="../assets/imgs/icons/nt-support__health.png" alt="">
+          <h4>Health insurance</h4>
+          <p>Suspendisse at nulla at tortor cursus feugiat. Praesent sed auctor felis.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'OurSupport',
+  props: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../scss/main.scss';
+
+.nt-support{
+  padding-top: 160px;
+
+  @media(max-width: 780px){
+    
+  }
+}
+</style>
